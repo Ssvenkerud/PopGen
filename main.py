4a2f04725ded91e6e4216fb7d40b2dec2d951649
Simen Svenkerud <s.svenkerud@gmail.com>
2022-04-18 15:16:51 +0200

    Implementet first runtime

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+from src.simple_generator import SimpleGenerator
 
 def main():
-    pass
-
+    population_size = int(input("enter population size"))
+    schema = {"integer": "int", "float": "float", "category": "cat"}
+    generator = SimpleGenerator(population_size, schema)
+    population = generator.run()
+    print(population.head())
 
 if __name__ == "__main__":
     main()
