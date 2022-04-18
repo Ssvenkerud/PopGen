64f33fd5c047cc9b24f8ce5f2bbded0db9b20d17
Ssvenkerud <Ssvenkerud@users.noreply.github.com>
2022-04-18 13:15:51 +0000

    Apply formatting standard

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 from src.simple_generator import SimpleGenerator
 
+
 def main():
     population_size = int(input("enter population size"))
     schema = {"integer": "int", "float": "float", "category": "cat"}
     generator = SimpleGenerator(population_size, schema)
     population = generator.run()
     print(population.head())
+
 
 if __name__ == "__main__":
     main()
